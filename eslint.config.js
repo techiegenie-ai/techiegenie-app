f9c2e0c31943d211a1cdf32702d70beb5262b999
kovalev-sergey <kovalevss@gmail.com>
2025-03-17 16:04:12 +0000

    feat: eslint

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginImport from "eslint-plugin-import";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["src/**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    ignores: ["eslint.config.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es6,
+        ...globals.node,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+        ecmaVersion: 2020,
+      },
+    },
+    settings: {
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"]
+        },
+        typescript: {
+          project: "./tsconfig.json"
+        }
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      import: pluginImport,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...pluginImport.configs.recommended.rules,
+      ...pluginImport.configs.typescript.rules,
+
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
+      ],
+    },
+  },
+];
